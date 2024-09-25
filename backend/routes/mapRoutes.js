5be0d7e548b9fca5f6ed4dba488013ff5ca2d631
Ethan Bagunas <epbagunas@gmail.com>
2024-09-26 06:29:48 +0800

    logs

--- backend/routes/mapRoutes.js
+++ backend/routes/mapRoutes.js
@@ -1,11 +1,12 @@
 var express = require('express');
 const router= express.Router();
 
-const {GetPosition, GetAllDetails}= require('../controllers/map');
+const {GetPosition, GetAllDetails, GetHistory}= require('../controllers/map');
 
 
 router.get('/list/:level', GetAllDetails);
 router.get('/marker/:level', GetPosition);
+router.get('history/:deviceId', GetHistory)
 
 const MapRoute = router;
 module.exports = MapRoute;
