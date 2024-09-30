3003fc4d127c21349c3c4d115a0030c6a9706b62
Ethan Bagunas <epbagunas@gmail.com>
2024-09-30 16:01:15 +0800

    added update device and status

--- frontend/src/components/Dashboard/AddDevices.jsx
+++ frontend/src/components/Dashboard/AddDevices.jsx
@@ -8,10 +8,11 @@ import TextField from '@mui/material/TextField';
 import { FormControl, FormLabel } from '@mui/material';
 import { Icon } from '@iconify/react';
 
-
-
-
-
+import { Tabs } from '@mui/base/Tabs';
+import { TabsList as BaseTabsList } from '@mui/base/TabsList';
+import { TabPanel as BaseTabPanel } from '@mui/base/TabPanel';
+import { buttonClasses } from '@mui/base/Button';
+import { Tab as BaseTab, tabClasses } from '@mui/base/Tab';
 
 const AddDevice = ({lat, lng, setLat, setLng}) => {
 
@@ -60,7 +61,74 @@ const AddDevice = ({lat, lng, setLat, setLng}) => {
         setLat(null);
         setLng(null);
     };
-  
+    const NewDevice= ()=>{
+      return(
+        <FormControl>
+                  <FormLabel>Click on the map to add device positions</FormLabel>
+                  <TextField
+                    id="deviceName"
+                    label="Device Name"
+                    name="deviceName"
+                    variant="outlined"
+                    onChange={handleInputChange}
+                    margin= "normal"
+                    />
+                  <TextField
+                    id="location"
+                    label="Location"
+                    name="location"
+                    variant="outlined"
+                    onChange={handleInputChange}
+                    margin= "normal"
+                    />
+                  <TextField
+                    id="latitude"
+                    label="Latitude"
+                    variant="outlined"
+                    value={lat}
+                    disabled
+                    margin= "normal"
+                    />
+                  <TextField
+                    id="longitude"
+                    label="Longitude"
+                    variant="outlined"
+                    value={lng}
+                    disabled
+                    margin= "normal"
+                    />
+                  <Button sx={{color:"#000000", backgroundColor: theme.palette.primary.main }} onClick={handleSubmit}>Submit</Button>
+      </FormControl>
+      )
+    } 
+    const UpdateDevice= ()=>{
+      return(
+        <FormControl>
+                  <FormLabel>Click on the map to add device positions</FormLabel>
+                  <TextField
+                    id="deviceName"
+                    label="Device Name"
+                    name="deviceName"
+                    variant="outlined"
+                    onChange={handleInputChange}
+                    margin= "normal"
+                    />
+                  <TextField
+                    id="location"
+                    label="Location"
+                    name="location"
+                    variant="outlined"
+                    onChange={handleInputChange}
+                    margin= "normal"
+                    />
+                  <Button sx={{color:"#000000", backgroundColor: theme.palette.primary.main }} onClick={handleSubmit}>Submit</Button>
+      </FormControl>
+      )
+    } 
+    const [tab, setTab]= useState(0);
+    const handleTab = (event, newValue) => {
+      setTab(newValue)
+    }
     return (
       <div>
         <Button onClick={handleAnchor} >
@@ -68,47 +136,21 @@ const AddDevice = ({lat, lng, setLat, setLng}) => {
         </Button>
         <BasePopup id={id} open={open} anchor={anchor} placement="right-end">
           <PopupBody> 
-          <FormControl>
-              <FormLabel>Click on the map to add device positions</FormLabel>
-              <TextField
-                id="deviceName"
-                label="Device Name"
-                name="deviceName"
-                variant="outlined"
-                onChange={handleInputChange}
-                margin= "normal"
-              />
-              <TextField
-                id="location"
-                label="Location"
-                name="location"
-                variant="outlined"
-                onChange={handleInputChange}
-                margin= "normal"
-              />
-              <TextField
-                id="latitude"
-                label="Latitude"
-                variant="outlined"
-                value={lat}
-                disabled
-                margin= "normal"
-              />
-              <TextField
-                id="longitude"
-                label="Longitude"
-                variant="outlined"
-                value={lng}
-                disabled
-                margin= "normal"
-              />
-              <Button sx={{color:"#000000", backgroundColor: theme.palette.primary.main }} onClick={handleSubmit}>Submit</Button>
-            </FormControl>
+            <Tabs defaultValue={1}>
+              <TabsList>
+                <Tab value={1}>Create Device</Tab>
+                <Tab value={2}>Update Device</Tab>
+              </TabsList>
+              <TabPanel value={1}><NewDevice/></TabPanel>
+              <TabPanel value={2}><UpdateDevice/></TabPanel>
+            </Tabs>
           </PopupBody>
         </BasePopup>
       </div>
     );
 }
+
+
 const grey = {
   50: '#F3F6F9',
   100: '#E5EAF2',
@@ -140,5 +182,82 @@ const PopupBody = styled('div')(
   z-index: 1;
 `,
 );
+
+const blue = {
+  50: '#F0F7FF',
+  100: '#C2E0FF',
+  200: '#80BFFF',
+  300: '#66B2FF',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0059B2',
+  800: '#004C99',
+  900: '#003A75',
+};
+
+
+const Tab = styled(BaseTab)`
+  font-family: 'IBM Plex Sans', sans-serif;
+  color: #fff;
+  cursor: pointer;
+  font-size: 0.875rem;
+  font-weight: 600;
+  background-color: transparent;
+  width: 50%;
+  padding: 10px 12px;
+  margin: 6px;
+  border: none;
+  border-radius: 7px;
+  display: flex;
+  justify-content: center;
+
+  &:hover {
+    background-color: ${blue[400]};
+  }
+
+  &:focus {
+    color: #fff;
+    outline: 3px solid ${blue[200]};
+  }
+
+  &.${tabClasses.selected} {
+    background-color: #fff;
+    color: ${blue[600]};
+  }
+
+  &.${buttonClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+const TabPanel = styled(BaseTabPanel)(
+  ({ theme }) => `
+  width: 100%;
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 0.875rem;
+  padding: 20px 12px;
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+  border-radius: 12px;
+  opacity: 0.6;
+  `,
+);
+
+const TabsList = styled(BaseTabsList)(
+  ({ theme }) => `
+  min-width: 400px;
+  background-color: ${blue[500]};
+  border-radius: 12px;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: space-between;
+  box-shadow: 0px 4px 30px ${theme.palette.mode === 'dark' ? grey[900] : grey[200]};
+  `,
+);
+
 
 export default AddDevice;
