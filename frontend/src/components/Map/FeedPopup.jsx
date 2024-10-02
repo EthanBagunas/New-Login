747c51729237d43f012926fa04a261e21f46270c
Ethan Bagunas <epbagunas@gmail.com>
2024-10-02 16:17:22 +0800

    feed popup init

---
+++ frontend/src/components/Map/FeedPopup.jsx
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import { Unstable_Popup as BasePopup } from '@mui/base/Unstable_Popup';
+import { styled } from '@mui/system';
+
+
+const grey = {
+  50: '#F3F6F9',
+  100: '#E5EAF2',
+  200: '#DAE2ED',
+  300: '#C7D0DD',
+  400: '#B0B8C4',
+  500: '#9DA8B7',
+  600: '#6B7A90',
+  700: '#434D5B',
+  800: '#303740',
+  900: '#1C2025',
+};
+
+const PopupBody = styled('div')(
+  ({ theme }) => `
+  width: max-content;
+  padding: 12px 16px;
+  margin: 8px;
+  border-radius: 8px;
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+  background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  box-shadow: ${
+    theme.palette.mode === 'dark'
+      ? `0px 4px 8px rgb(0 0 0 / 0.7)`
+      : `0px 4px 8px rgb(0 0 0 / 0.1)`
+  };
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-weight: 500;
+  font-size: 0.875rem;
+  z-index: 1;
+`,
+);
+
+const FeedPopup = (anchor) => {
+
+  const open = Boolean(anchor);
+  const id = open ? 'simple-popup' : undefined;
+  
+    return(
+      <BasePopup id={id} open={open} anchor={anchor} >
+          <PopupBody>
+            watashi wa FeedPopup from Popup.jsx. edit me senpai 
+          </PopupBody>      
+      </BasePopup>
+    )
+  }
+
+export default FeedPopup;
