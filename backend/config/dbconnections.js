99c33abdd1c5df5f5398aff63af8a4a331fbc576
Ethan Bagunas <epbagunas@gmail.com>
2024-10-06 22:07:05 +0800

    connected to the cloud

--- backend/config/dbconnections.js
+++ backend/config/dbconnections.js
@@ -1,10 +1,22 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+module.exports = db
+
+/* for the localhost implementation
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "crud"
-
+    database: ""
 })
-module.exports = db
+*/
