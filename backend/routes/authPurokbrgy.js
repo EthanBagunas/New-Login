ee9d2988237eb466c30a1977f9fa0d219f5e3f82
JuneRaz <floresjunmar1@gmail.com>
2024-10-26 13:24:34 +0800

    WIth UI and Add admin User

--- backend/routes/authPurokbrgy.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const GetBrgyNamesController = require('../controllers/Settings/Purok/getBrgyController');
-
-router.get('/', GetBrgyNamesController.GetBrgyPurok);
-
-module.exports = router;
